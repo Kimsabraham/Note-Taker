f782a46277bc60c9cab5ebfa77f9fef2be84e68f
Abraham Kim <krnpride730@yahoo.com>
2021-04-29 16:22:45 -0400

    connected to heroku but hit error when deployed

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 
 var express = require("express");
 
-
+// express port
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
+// Data parce
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/htmlRoute")(app);
-require("./routes/apiRoute")(app);
 
 
+// express listening for port 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
