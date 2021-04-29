c90e5abb76dfd65e8765a84540351e71d67e92e1
Abraham Kim <krnpride730@yahoo.com>
2021-04-29 16:19:11 -0400

    lining up correct variables with classes

--- server.js
+++ server.js
@@ -10,5 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+require("./routes/htmlRoute")(app);
+require("./routes/apiRoute")(app);
 
 
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
