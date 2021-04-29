a5e956808b12dc0900456a0fbcc0fc55d4bcdf47
Abraham Kim <krnpride730@yahoo.com>
2021-04-29 16:44:07 -0400

    heroku deployed but notes html not populating correctly

--- routes/routes.js
+++ routes/routes.js
@@ -2,52 +2,49 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = (app) => {
-  // Setup notes variable
+  //notes variable
   fs.readFile("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
 
     var notes = JSON.parse(data);
 
-    // API ROUTES
-    // ========================================================
-
-    // Setup the /api/notes get route
+    
+    //API get route
     app.get("/api/notes", function (req, res) {
-      // Read the db.json file and return all saved notes as JSON.
+     
       res.json(notes);
     });
 
-    // Setup the /api/notes post route
+    // API post route
     app.post("/api/notes", function (req, res) {
-      // Receives a new note, adds it to db.json, then returns the new note
+
+      
       let newNote = req.body;
       notes.push(newNote);
       updateDb();
       return console.log("Added new note: " + newNote.title);
     });
 
-    // Retrieves a note with specific id
+    // retrieves note by specific id
     app.get("/api/notes/:id", function (req, res) {
-      // display json for the notes array indices of the provided id
+     
       res.json(notes[req.params.id]);
     });
 
-    // Deletes a note with specific id
+    // deletes note by specific id
     app.delete("/api/notes/:id", function (req, res) {
       notes.splice(req.params.id, 1);
       updateDb();
       console.log("Deleted note with id " + req.params.id);
     });
 
-    // VIEW ROUTES
-    // ========================================================
-
-    // Display notes.html when /notes is accessed
+   
+    // display note.html
     app.get("/notes", function (req, res) {
       res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
-    // Display index.html when all other routes are accessed
+    // display index.html
     app.get("*", function (req, res) {
       res.sendFile(path.join(__dirname, "../public/index.html"));
     });
